1d28548469a70b97601d504dc37c07eb002827a7
Ashwini <kakdeashwini12@gmail.com>
2022-07-04 10:16:12 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+# *
+# 'hello'
+# -87.8
+# -
+# /
+# +
+# 6
+# 
+# Ans-->expression are  *,-,/,+ and values are 'hello' -87.8
+
+# 2. What is the difference between string and variable?
+# A Variable is a store of information, and a String is a type of information you would store in a Variable.
+
+# 3. Describe three different data types.
+# Ans- String ,Integer, Boolean 
+
+# 4. What is an expression made up of? What do all expressions do?
+# An expression is a construct made up of variables, operators, and method invocations, which are constructed according to the syntax of the language, that evaluates to a single value
+
+# 5.This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement? 
+# Ans -> when it is acting like expression it compare the two values and statement it assigns a value
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# Ans =23
+
+# 7. What should the values of the following two terms be?
+# 'spam'+'spamspam'--->'spamspamspam'
+# 'spam'*3--->'spamspamspam'
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# Ans->because 100 is a int value and we cant assign numeric data as a variable
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+# Ans->int(),float(),str() are used 
+
+# In[ ]:
+
+
+get_ipython().set_next_input('10. Why does this expression cause an error? How can you fix it');get_ipython().run_line_magic('pinfo', 'it')
+
